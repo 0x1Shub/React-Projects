59047c1cff1487497e3a11ec291ac8871b8c2239
0x1Shub <shubhamyeram13@gmail.com>
2024-10-15 23:19:25 +0530

    Created: React projects

---
+++ 05_Notes_App/src/components/NoteItem.jsx
@@ -0,0 +1,16 @@
+// src/components/NoteItem.jsx
+import React from "react";
+
+const NoteItem = ({ note, onDelete, onEdit }) => {
+  return (
+    <div className="note-item">
+      <p>{note.text}</p>
+      <div>
+        <button onClick={() => onEdit(note)}>Edit</button>
+        <button onClick={() => onDelete(note.id)}>Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default NoteItem;
